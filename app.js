37f9bfe3b195c762ea015e59b3d1b2693e68d374
Kelsey Bones <kelseybones@hotmail.co.uk>
2015-12-29 19:32:23 +0000

    Clean up Code
    Changed if to switch statement.

--- app.js
+++ app.js
@@ -59,66 +59,55 @@ function setIconAndDescription(weather, location){
 
 	var icon;
 	var description;
+    var backgroundColour;
 	
 	weather = weather.toLowerCase();
-		
-	if (weather == "clear sky"
-		|| weather == "clear"){
-		
-		icon = "clear.svg";
-		description = "It’s a qwer day out there.";
-		
-	}
-	else if (weather == "few clouds"){
-		
-		icon = "few-clouds.svg";
-		description = "Aye she’s no a bad day";
-		
-	}
-	else if (weather == "scattered clouds" 
-		|| weather == "broken clouds" 
-		|| weather == "clouds"){
-		
-		icon = "clouds.svg";
-		description = "She’s a dull oul’ day";
-		
-	}
-	else if (weather == "rain"
-	|| weather == "light rain"
-	|| weather == "shower rain"){
-		
-		icon = "rain.svg";
-		description = "It’s lashing down."
-		
-	}
-	else if(weather == "thunderstorm"){
-		
-		icon = "thunder.svg";
-		description = "It’s a wile day."
-		
-	}
-	else if (weather == "snow"){
-		
-		icon = "snow.svg";
-		description = "Ye wouldne be long gettin’ frostbit."
-		
-	}
-	else if (weather == "mist"){
-		
-		icon = "mist.svg";
-		description = "She’s no great.";
-		
-	}
-	else {
-		
-		icon = "default.svg";
-		description = "Oops, I couldn't find the weather in " + location;
-		
-	}
+    
+    switch(weather) {
+        case "clear":
+        case "clear sky":
+            icon = "clear.svg";
+            description = "It’s a qwer day out there.";
+            backgroundColour = "-webkit-linear-gradient(left top, #001422, #054a5c);";
+            break;
+        case "few clouds":
+            icon = "few-clouds.svg";
+            description = "Aye she’s no a bad day";
+            break;
+        case "scattered clouds":
+        case "broken clouds":
+        case "clouds":
+            icon = "clouds.svg";
+            description = "She’s a dull oul’ day";
+            break;
+        case "rain":
+        case "light rain":
+        case "shower rain":
+            icon = "rain.svg";
+            description = "It’s lashing down."
+            break;
+        case "thunderstorm":
+            icon = "thunder.svg";
+            description = "It’s a wile day."
+            break;
+        case "snow":
+            icon = "snow.svg";
+            description = "Ye wouldne be long gettin’ frostbit."
+            break;
+        case "mist":
+            icon = "mist.svg";
+            description = "She’s no great.";
+            break;
+        default:
+            icon = "default.svg";
+            description = "Oops, I couldn't find the weather in " + location;
+            backgroundColour = "-webkit-linear-gradient(left top, #001422, #054a5c);";
+            break;
+    }
 
 	document.getElementById("weatherIcon").src = "images/" + icon;
 	document.getElementById("description").innerHTML = description;
-	
+//    document.body.style.backgroundColor = backgroundColour;
 }
 
 (function() {
